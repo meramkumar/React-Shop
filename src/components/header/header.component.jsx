f61882b8ea0acda60a8d6342cfe4f852451edead
meramkumar <meramkumar@gmail.com>
2020-05-28 01:39:08 -0400

    Included Root Reducer and Middleware

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.style.scss';
@@ -23,11 +23,16 @@ const Header = ({ currentUser }) =>
         </Link>
                 {
                     currentUser ?
-                        <div className='option' onClick={ () =>auth.signOut() }>SIGN OUT</div> :
+                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
                         <Link className='option' to='/SignIn'>SIGN IN</Link>}
 
             </div>
         </div>
-    )
+    );
 
-export default Header;
+const mapStateToProps = state => {
+    currentUser: state.user.currentUser
+}
+
+
+export default connect(mapStateToProps) (Header);
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -3,17 +3,17 @@ const INITIAL_STATE = {
 }
 
 
-const useReducer = (state = INITIAL_STATE, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
         switch (action.type) {
                 case 'SET_CURRENT_USER':
                         return {
                                 ...state,
                                 currentUser: action.payload
-                        }
+                        };
 
                 default:
                         return state;
         }
 }
 
-export default useReducer;
+export default userReducer;
