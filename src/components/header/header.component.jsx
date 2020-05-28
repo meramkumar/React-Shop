5c98b84f9844b67de5709f70a6d2ebc7876ef468
meramkumar <meramkumar@gmail.com>
2020-05-28 01:59:35 -0400

    Updated header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.style.scss';
+
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -30,9 +31,9 @@ const Header = ({ currentUser }) =>
         </div>
     );
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
     currentUser: state.user.currentUser
-}
+});
 
 
-export default connect(mapStateToProps) (Header);
+export default connect(mapStateToProps)(Header);
