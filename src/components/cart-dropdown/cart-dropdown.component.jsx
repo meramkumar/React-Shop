6c6463cce439e9989aa40f27c05790bf74b80a27
meramkumar <meramkumar@gmail.com>
2020-06-22 23:07:19 -0400

    display the value with decimal values.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,21 +6,26 @@ import './cart-dropdown.styles.scss';
 import CustomButton from '../form-button/form-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-const CartDropDown = ({ cartItems }) =>
+const CartDropDown = ({ cartItems, totalSum }) =>
     (<div className='cart-dropdown'>
             <div className='cart-items'>
                 {
                     cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem}></CartItem>))
                 }
         </div> 
-        
+        <span className='total' >Total : ${totalSum.toFixed(2)}</span>
             <CustomButton>Go to Checkout</CustomButton>
         </div>
     );
 
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+    cartItems,
+    totalSum: cartItems.reduce(
+        (totalSum, cartItem) => totalSum + (cartItem.quantity * cartItem.price), 0)
 });
+
+ 
+
 
 export default connect(mapStateToProps)(CartDropDown);
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -7,7 +7,7 @@ const CartItem = ({ item: {imageUrl, price, name, quantity } }) => (
         <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
-            <span className='price' >{quantity} X ${price} : ${quantity*price} </span>
+            <span className='price' >{quantity} X ${price.toFixed(2)}</span>
 
         </div>
     </div>
