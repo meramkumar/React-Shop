1a66780fb0e10ed27999c2975889bf274b1acf38
meramkumar <meramkumar@gmail.com>
2020-06-24 01:26:53 -0400

    Included delete icon using material UI

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import './cart-item.style.scss';
 
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+
 const CartItem = ({ item: {imageUrl, price, name, quantity } }) => (
     <div className='cart-item'>
         <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price' >{quantity} X ${price.toFixed(2)}</span>
-
+        </div>
+        <div className='item-remove'>
+            <DeleteForeverIcon />
         </div>
     </div>
 )
